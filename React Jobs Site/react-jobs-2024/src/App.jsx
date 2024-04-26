01363a515c5580b8433baea984be5c71bdd4e89b
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-04-26 16:55:38 -0600

    Trying out jsx

--- React Jobs Site/react-jobs-2024/src/App.jsx
+++ React Jobs Site/react-jobs-2024/src/App.jsx
@@ -1,6 +1,28 @@
 const App = () => {
+  const name = 'Gerard';
+  const age = 20;
+  const preferences = ['cats', 'dogs', 'food'];
+  const loggedIn = true;
+
+  const styles = {
+    color: 'green',
+    fontFamily: 'Robloto',
+    fontSize: '55px',
+  };
+
   return (
-    <div className="text-5xl">App</div>
+    <>
+      <div className="text-5xl">App</div>
+      <p style={{ color: 'red', fontFamily: 'arial'}}>Welcome</p>
+      <p>Hello {name}, your age is {age}</p>
+      <p>Here are your preferences:</p>
+      <ul>
+        {preferences.map((names, index) => (
+          <li key={index} style={styles}>{names}</li>
+        ))}
+      </ul>
+      { loggedIn && <h1>Hello Member</h1>}
+    </>
   )
 }
 
