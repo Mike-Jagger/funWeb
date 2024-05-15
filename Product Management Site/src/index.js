79beb53445d7b762821b02cef2f7b5e32debda46
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-05-15 10:17:03 -0600

    Adding project files - Product Management Site

---
+++ Product Management Site/src/index.js
@@ -0,0 +1,23 @@
+// index.js
+// Name: Mohamad Ghattas
+// UCID: 30194874
+// Name: Gerard Ledoux Mendjemo Ngangom
+// UCID: 30192835
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # funWeb
 Building some really cool web dev projects
+
+> React Jobs Site
+> Restful API for products
+> Mapbox Boostrap 5 Site
+> React E-commerce Site
+> Understanding System Design
+> Python Microservices App
+> Automate with python
